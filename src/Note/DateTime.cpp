76def59981af55cf717754b524a66c7d060ce4df
AdminFedora <segor6052@gmail.com>
2025-01-18 15:39:29 +0200

    0.1

--- src/Note/DateTime.cpp
+++ src/Note/DateTime.cpp
@@ -4,13 +4,10 @@
 #include <iomanip>
 #include <sstream>
 
-
 std::string Note::getCurrentDateTime() {
-    // Get the current time
     std::time_t t = std::time(nullptr);
     std::tm* now = std::localtime(&t);
 
-    // Formatting time and date
     std::ostringstream oss;
     oss << std::put_time(now, "%H:%M %d:%m:%Y");
     return oss.str();
